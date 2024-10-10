11b05c61bc5d1d4a94e9ffc03dc2d663f3684518
kongsun leu <kongsun.leu@pipay.local>
2024-10-10 20:11:11 +0700

    init

---
+++ src/public/icons/SkillIcon.tsx
@@ -0,0 +1,19 @@
+const SkillIcon = () => (
+  <svg
+    width="18px"
+    height="18px"
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M9 8L5 11.6923L9 16M15 8L19 11.6923L15 16"
+      stroke="#14B8A6"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+  </svg>
+);
+
+export default SkillIcon;
