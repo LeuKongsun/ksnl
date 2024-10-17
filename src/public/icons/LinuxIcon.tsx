3c1bea6acfc07de1890a7d92ee41130270294533
kongsun leu <kongsun79@gmail.com>
2024-10-17 15:15:57 +0700

    fix(svg-attr): update linux svg attribute

--- src/public/icons/LinuxIcon.tsx
+++ src/public/icons/LinuxIcon.tsx
@@ -10,11 +10,11 @@ const LinuxIcon: React.FC<IconProps> = ({ className }) => {
       width="100%"
       height="100%"
     >
-      <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+      <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
       <g
         id="SVGRepo_tracerCarrier"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       ></g>
       <g id="SVGRepo_iconCarrier">
         <path
